2541101c1f68cf3961c87efa4dbfae7fe1149044
matvimykula <matt.pronchick@gmail.com>
2023-04-29 10:45:26 -0700

    fly changes

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -2,11 +2,12 @@ function figureAPI() {
   //   console.log(window.location);
   console.log(process.env.NODE_ENV);
   const devBackend = 'http://localhost:8080/';
-  const prodBackend = 'http://icy-river-1119.fly.dev'; ///replace with new url
+  const prodBackend = 'https://icy-river-1119.fly.dev'; ///replace with new url
 
   const prodEnv = process.env.NODE_ENV === 'production';
   let environment;
   prodEnv ? (environment = prodBackend) : (environment = devBackend);
+  console.log({ prodEnv });
   return environment;
 }
 
