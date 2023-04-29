05514d2a19d8cb2c7dde54ecd62c4f17252f8773
matvimykula <matt.pronchick@gmail.com>
2023-04-29 14:58:41 -0700

    fly change

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -2,7 +2,7 @@ function figureAPI() {
   //   console.log(window.location);
   console.log(process.env.NODE_ENV);
   const devBackend = 'http://localhost:8080/';
-  const prodBackend = 'https://icy-river-1119.fly.dev'; ///replace with new url
+  const prodBackend = 'solitary-wildflower-8091.fly.dev'; ///replace with new url
 
   const prodEnv = process.env.NODE_ENV === 'production';
   let environment;
