e22a31242dc9abd1879f046847ae5459afc9600c
matvimykula <matt.pronchick@gmail.com>
2023-05-11 16:38:59 -0700

    accidental fly change

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -56,6 +56,7 @@ function Home() {
 
   return (
     <Box className={styles.homepage}>
+      {/* <Button onClick={() => socket.emit('create:poem', key)}></Button> */}
       <SegmentedControl
         style={{ border: '2px solid black' }}
         color="blue"
--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -2,7 +2,7 @@ function figureAPI() {
   //   console.log(window.location);
   console.log(process.env.NODE_ENV);
   const devBackend = 'http://localhost:8080/';
-  const prodBackend = 'solitary-wildflower-8091.fly.dev'; ///replace with new url
+  const prodBackend = 'purple-snow-4969.fly.dev'; ///replace with new url
 
   const prodEnv = process.env.NODE_ENV === 'production';
   let environment;
